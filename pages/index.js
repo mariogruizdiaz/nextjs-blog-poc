3be923a9120088d39feb4f05fc299f59bedea281
Mario Ruiz Diaz <mariogrd@gmail.com>
2023-09-20 14:45:30 -0300

    profile updates

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+      <p>With over two decades of hands-on experience in technology, I have embraced the roles of a Technologist, Senior Full Stack Developer, and Hands-On Software Architect. My journey reflects a deep passion for innovative design and an extensive skill set that spans 20 years of expertise in JavaScript, Reactjs, Nodejs, React Native, Docker, Kubernetes, Observability, and other cutting-edge tools. Throughout my career, I have thrived as a Tech Lead, Principal Software Engineer, and AWS Engineer, consistently driving projects to success and delivering high-quality solutions. Let's collaborate to shape the future of technology together</p>
         <p>
           (This is a sample website - you’ll be building a site like this in{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
